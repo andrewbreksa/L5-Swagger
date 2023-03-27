79f012018aae2194bee65aba0b6b8cabcb495c9a
Andrew Breksa <andrew.breksa@wagwalking.com>
2023-03-27 13:23:14 -0700

    [7.0.1-php8] remove symfony/yaml `dump` flags

--- src/Generator.php
+++ src/Generator.php
@@ -208,10 +208,7 @@ class Generator
             file_put_contents(
                 $this->yamlDocsFile,
                 (new YamlDumper(2))->dump(
-                    json_decode(file_get_contents($this->docsFile), true),
-                    20,
-                    0,
-                    Yaml::DUMP_OBJECT_AS_MAP | Yaml::DUMP_EMPTY_ARRAY_AS_SEQUENCE
+                    json_decode(file_get_contents($this->docsFile), true), 20
                 )
             );
         }
